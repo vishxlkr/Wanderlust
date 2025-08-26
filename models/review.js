5ecc4787f5e3c1cabc6c0b18c74cdc9399a88494
vishxlkr <vishxlkr@gmail.com>
2025-08-27 00:47:37 +0530

    update

--- models/review.js
+++ models/review.js
@@ -2,20 +2,24 @@ const { string, number, ref } = require("joi");
 const mongoose = require("mongoose");
 
 const ReviewSchema = mongoose.Schema({
-  comment: String,
-  rating: {
-    type: Number,
-    min: 1,
-    max: 5,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  author: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-  },
+   comment: {
+      type: String,
+      required: true, // optional, but usually reviews must have text
+   },
+   rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: true,
+   },
+   createdAt: {
+      type: Date,
+      default: Date.now,
+   },
+   author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+   },
 });
 
 module.exports = mongoose.model("Review", ReviewSchema);
