58fbfbee85193bc7c2b248ba993a1c99aea15468
vishxlkr <vishxlkr@gmail.com>
2025-08-27 00:21:46 +0530

    update

--- middleware.js
+++ middleware.js
@@ -55,8 +55,8 @@ module.exports.isOwner = async (req, res, next) => {
 
 module.exports.isReviewAuthor = async (req, res, next) => {
    let { id, reviewId } = req.params;
-   let review = await Review.findById(reviewId);
-   if (!review.author.equals(res.locals.currUser._id)) {
+   let listing = await Review.findById(reviewId);
+   if (!listing.author.equals(res.locals.currUser._id)) {
       req.flash("error", "You are not the author of this review !");
       return res.redirect(`/listings/${id}`);
    }
